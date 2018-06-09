ac1b6f6f8a1ea175e255340c679729a59d998e5f
usbpendrive <ferry.fernando@gmail.com>
2018-06-09 08:23:12 +0700

    Using ES2015.

--- static/App.js
+++ static/App.js
@@ -1,7 +1,15 @@
+'use strict';
+
 var contentNode = document.getElementById('contents');
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var message = continents.map(function (c) {
+  return 'Hello ' + c + '!';
+}).join(' ');
+
 var component = React.createElement(
-  'h1',
+  'p',
   null,
-  'Hello World!'
+  message
 );
 ReactDOM.render(component, contentNode);
